796be85356c6e13b6cfbbe3b30f2de1781f9e90d
hardeepsinhraj <hkraj0209@gmail.com>
2025-04-04 11:19:07 -0400

    completed lesson-12.

--- lesson-12/js/tables.js
+++ lesson-12/js/tables.js
@@ -1,1 +1,4 @@
 // STEP 11: Call to tablesorter script here
+const consoleTable = document.querySelector("#console-table");
+//enabel TableSort.js on the taeble
+let myTable = new Tablesort(consoleTable);
